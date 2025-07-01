2cfd7bc4ab5523d3b49a8bf3033014cc66d4b32e
Evgeniaevz <tinroom@mail.ru>
2025-07-01 15:19:49 +0300

    Изменение App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import HelpPage from './pages/HelpPage';
 
 function App() {
     return (
-        <Router>
+        <Router basename="/Site-about-Dolphins">
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/evolution" element={<EvolutionPage />} />
